978816e4af42ad4fbca15f49171894682bc2466b
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2022-08-15 03:42:10 -0500

    ok

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,9 +3,17 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="loading">
-      <img src="/src/assets/img/title.png" alt="" className="loading__title" />
+      <img
+        src="https://i.postimg.cc/RFg6kr73/title.png"
+        alt="Title"
+        className="loading__title"
+      />
       <div className="loading__circle">
-        <img src="/src/assets/img/LoadIcon.png" alt="" srcset="" />
+        <img
+          src="https://i.postimg.cc/MG8QRmZG/LoadIcon.png"
+          alt="Loading-icon"
+          srcset=""
+        />
       </div>
     </div>
   );
